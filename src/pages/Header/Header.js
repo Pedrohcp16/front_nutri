be9239dc70b0d330dfa053f7360663f6e4beb84b
Pedrohcp16 <pedrohcp16@outlook.com>
2025-04-15 01:07:05 -0300

    comit2

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -9,14 +9,16 @@ const Header = () => {
         <nav className="navigation">
           <a href="#sobre">Sobre</a>
           <a href="#consulta">Consulta</a>
-          <a href="#duvidas-frequentes">Duvidas frequentes</a>
+          <a href="#duvidas-frequentes">Dúvidas Frequentes</a>
           <a href="#contato">Contato</a>
         </nav>
-        <div className="social-media">
-          <a href="https://js.pngtree.com/a5/static/qc26tp.DsQm_9BT.jpg" target="_blank" rel="noopener noreferrer">
-            <img src='./assets/images/instagram_logo.png'alt="Instagram" />
-            
-          </a>
+        <div className="right-section">
+          <div className="social-media">
+            <a href="https://js.pngtree.com/a5/static/qc26tp.DsQm_9BT.jpg" target="_blank" rel="noopener noreferrer">
+              <img src='./assets/images/instagram_logo.png' alt="Instagram" />
+            </a>
+          </div>
+          <button className="login-button">Login</button>
         </div>
       </div>
     </header>
